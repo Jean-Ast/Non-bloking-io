c584d38b39a68d750a3bffa2996830034fe4a27c
Jean <jeanastacio31@gmail.com>
2021-04-21 13:26:23 -0400

    Getting all images but not syncronous

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,64 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/png"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
+
+type Image struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Src  string `json:"src"`
+}
+
+var Images []Image
+
+func returnAllImages(w http.ResponseWriter, r *http.Request) {
+	// Read image from file that already exists
+	existingImageFile, err := os.Open("test.png")
+	if err != nil {
+		// Handle error
+	}
+	defer existingImageFile.Close()
+
+	// Calling the generic image.Decode() will tell give us the data
+	// and type of image it is as a string. We expect "png"
+	imageData, _, err := image.Decode(existingImageFile)
+	if err != nil {
+		// Handle error
+	}
+	fmt.Println(imageData)
+	// fmt.Println(imageType)
+
+	// We only need this because we already read from the file
+	// We have to reset the file pointer back to beginning
+	existingImageFile.Seek(0, 0)
+
+	// Alternatively, since we know it is a png already
+	// we can call png.Decode() directly
+	loadedImage, err := png.Decode(existingImageFile)
+	if err != nil {
+		// Handle error
+	}
+	fmt.Println(loadedImage)
+
+	//Returns images
+	// vars := mux.Vars(r)
+	// key := vars["id"]
+
+	// for _, image := range Images {
+	// 	if image.Id == key {
+	// 		json.NewEncoder(w).Encode(image)
+	// 	}
+	// }
+	// json.NewEncoder(w).Encode(Images)
+	fmt.Println("Endpoint Hit: returnImages")
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -19,21 +72,16 @@ func handleRequests() {
 
 	//GET requests
 	myRouter.Handle("/", http.HandlerFunc(homePage)).Methods("GET")
-	myRouter.Handle("/", http.HandlerFunc(returnImage)).Methods("GET")
+	myRouter.Handle("/api/images", http.HandlerFunc(returnAllImages)).Methods("GET")
 
-	//POST requests
-	// myRouter.Handle("/", http.HandlerFunc()).Methods("POST")
-
-	//PUT requests
-	// myRouter.Handle("/}", http.HandlerFunc()).Methods("PUT")
-
-	//DELETE requests
-	// myRouter.Handle("/", http.HandlerFunc()).Methods("DELETE")
-
-	// start server on port 10000 and binds to myRouter
+	// start server on port 8080 and binds to myRouter
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 func main() {
 	fmt.Println("RESTful API up!")
+	Images = []Image{
+		{Id: "1", Name: "Facebook", Src: "images\test.png"},
+		{Id: "2", Name: "Mario", Src: "images\test2.png"},
+	}
 	handleRequests()
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.15
 
 require (
 	github.com/amalfra/etag v0.0.0-20201123174553-83e6a87e7d0d // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20210420193930-a4630ec28c79 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/go-version v1.3.0 // indirect
 	gorm.io/driver/clickhouse v0.1.0 // indirect
 	// gorm.io/driver/sqlserver v1.0.7 // indirect
 	gorm.io/gorm v1.21.6 // indirect
+	honnef.co/go/js/util v0.0.0-20150216223935-96b8dd9d1621 // indirect
+	honnef.co/go/js/xhr v0.0.0-20150307031022-00e3346113ae // indirect
 )
